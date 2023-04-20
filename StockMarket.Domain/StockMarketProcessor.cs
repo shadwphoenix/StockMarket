c7e1a8dc0d771e213d49991f6cb4e97ebb64bc61
negararj <negararj1382@gmail.com>
2023-04-20 20:37:46 +0430

    end of session 1 20 april 2022

--- StockMarket.Domain/StockMarketProcessor.cs
+++ StockMarket.Domain/StockMarketProcessor.cs
@@ -4,20 +4,40 @@
     {
 
         private long lastOrderId;
-
+        private readonly PriorityQueue<Order,Order> buyOrders;
+        private readonly PriorityQueue<Order,Order> sellOrders;
         public StockMarketProcessor(long lastOrderId = 0)
         {
             this.lastOrderId = lastOrderId;
+            buyOrders = new PriorityQueue<Order,Order>();
+            sellOrders = new PriorityQueue<Order,Order>();
+
         }
 
         public void EnqueueOrder(TradeSide side, decimal quantity, decimal price)
         {
             if (side == TradeSide.Buy)
             {
-                lastOrderId++;
+                Interlocked.Increment(ref lastOrderId);
                 Order order = new(lastOrderId, side, quantity, price);
+                ProcessBuyOrder(order);
             }
+            else
+            {
+                Interlocked.Increment(ref lastOrderId);
+                Order order = new(lastOrderId, side, quantity, price);
+                ProcessSellOrder(order);
+            }
+            throw new NotImplementedException();
+        }
 
+        private void ProcessSellOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ProcessBuyOrder(Order order)
+        {
             throw new NotImplementedException();
         }
     }
